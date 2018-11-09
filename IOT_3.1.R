9bde69a751fdd67b25b3fe63fbe9d866f59c601d
Joana Cruz Ferreira <cfn.joana@gmail.com>
2018-11-09 18:57:24 +0100

    Some generated variables

--- IOT_3.1.R
+++ IOT_3.1.R
@@ -25,7 +25,7 @@ install.packages("dplyr")
 install.packages("tidyr")
 library(dplyr)
 library(tidyr)
-
+library(chron)
 #### 2. Upload dataset ####
 household_power_consumption <- read.csv("~/Desktop/UBIQUM/2. Tasks/Course 3/Task 1/Data/household_power_consumption.txt", 
                                         sep=";", dec = ".", na.strings = c("NA"," ","?"), stringsAsFactors = FALSE)
@@ -53,3 +53,39 @@ PowerConsumption$DateTime <- strptime(PowerConsumption$DateTime, "%d/%m/%Y %H:%M
 str(PowerConsumption)
 PowerConsumption$Date <- as.Date(PowerConsumption$Date, "%d/%m/%Y")
 str(PowerConsumption)
+
+substr(PowerConsumption[1,]$Time,1,2)
+
+#### 5. Generate new variables ####
+
+#5.1 Global_apparent_power
+PowerConsumption$Global_apparent_power=sqrt((PowerConsumption$Global_active_power^2)+(PowerConsumption$Global_reactive_power^2))
+str(PowerConsumption)
+PowerConsumption[1,]
+
+#5.2 Total_Sub_metering
+PowerConsumption$Global_Sub_metering <- PowerConsumption$Sub_metering_1 + PowerConsumption$Sub_metering_2 +PowerConsumption$Sub_metering_3
+str(PowerConsumption)
+
+#5.3 Hour
+substr(PowerConsumption[1,]$Time, 1,2)
+PowerConsumption$Hour <- substr(PowerConsumption$Time, 1,2)
+str(PowerConsumption)
+
+PowerConsumption$Hour<-as.numeric(PowerConsumption$Hour)
+is.numeric(PowerConsumption$Hour)
+
+#5.4 No_Sub_metering_Energy
+PowerConsumption$No_Sub_metering_Energy<- PowerConsumption$Global_active_power*1000/60 - (PowerConsumption$Sub_metering_1 + PowerConsumption$Sub_metering_2 +PowerConsumption$Sub_metering_3)
+
+#5.5 Period of the day
+
+#5.6 Season of the year
+
+
+#### 6. Treat missing values ####
+# The dataset contains some missing values in the measurements (nearly 1,25% of the rows). All calendar timestamps are present in the dataset but for some timestamps, the measurement values are missing: a missing value is represented by the absence of value between two consecutive semi-colon attribute separators. For instance, the dataset shows missing values on April 28, 2007.
+
+#### 7. Deal with timezone ####
+# France
+
