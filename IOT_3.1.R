2f6b1f40a35d6b92e29cf61b6ffcb94e0cff96c8
Joana Cruz Ferreira <cfn.joana@gmail.com>
2018-11-09 13:03:33 +0100

    Understand business problem / analysis goal

---
+++ IOT_3.1.R
@@ -0,0 +1,20 @@
+#### TASK 3.1 #####
+# Define a Data Science Process
+# Joana
+# 09/11 - 14/11
+# IOT Analytics
+
+#### General goal ####
+# Develop analytics for a new set of electrical sub-metering devices, which measure power consumption and energy usage. 
+# To create "smart homes" that provide home owners with analytics on their power usage.
+
+#### Specific goal #### 
+# Provide economic incentive recommendations to homeowners.
+# The company and its customers need to understand: if benefit of installing sub-metering devices justifies its cost.
+
+#### Deliverable: ####
+# Written ppt report that will be delivered/ presented to their management team (business people);
+# Tell them how will we conduct the analysis and what they are likely to gain
+
+
+rm(list=ls())
